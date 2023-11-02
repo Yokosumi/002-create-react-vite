b6680be13ac9f3dc3efa306d9ecea1777e1a96cf
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-02 13:11:27 +0100

    employees in

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import employees from "./data/employees.json";
+
 function App() {
+	const title = "Employee Directory";
+	const introText =
+		"Welcome to our Employee Directory, your one-stop destination for discovering and connecting with the talented individuals who make our organization thrive.";
 	return (
 		<>
-			<h1>employee site</h1>
+			<h1 className="text-3xl mb-3">{title}</h1>
+			<p>{introText}</p>
+			<hr />
 		</>
 	);
 }
